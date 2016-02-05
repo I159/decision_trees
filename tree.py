cc1384a954ecd1ccb29339e2540f8c4fd0a5a0ab
Ilya Pekelny <pekelny@gmail.com>
2016-02-05 18:20:16 +0200

    Entropy computation

--- tree.py
+++ tree.py
@@ -2,6 +2,8 @@
 
 All the features including a target feature is binary."""
 
+import math
+
 
 class Tree(object):
     """Decision tree controller object.
@@ -10,27 +12,42 @@ class Tree(object):
 
     def __init__(self, learning_data, target):
         # * Sort a training data relatively to a target feature to determine
-        # * the most bound features: the smaller entropy at the data sorted
+        # the most bound features: the smaller entropy at the data sorted
         # relative to the target feature, the more bound the feature to the
         # target.
-        self.learning_data = sorted(learning_data, key=target)
+        self.learning_data = sorted(learning_data, key=lambda x: x[target])
         self.target = target
         self.learn()
         self.cleanup()
 
-    def count_entropy(self, from_, to):
-        # * Count entropy of a left part relatively to a target key
-        # * Count entropy of a right part relatively to a target key
-        # * Count entropy of a left part relatively to an unknown key
-        # * Count entropy of a right part relatively to an unknown key
-        # * Count average entropy.
-        # * return slice and entropy
+    def get_probs(self, key, from_, to):
+        """Get probability for a different values of a key on a slice."""
+        the_slice = self.learning_data[from_: to]
+        # This example is binary. With non binary data it
+        # could be much more complex.
+        for i in (0, 1):
+            by_key = filter(lambda x: x[key] == i, the_slice)
+            yield len(by_key) / float(len(the_slice))
+
+    def cnt_entp(self, key, from_, to):
+        """Count entropy for a key on a slice."""
+        probs = self.get_probability(key, from_, to)
+        return sum(map(lambda p: -(p * math.log(p, 2)), probs))
+
+    def min_ave_entropy_idx(self, key, from_, to):
+        # * Conunt average entropy for all allowed slices
+        # * Return a minimum average entropy index and a prevailing
+        # values of the right and left side by the target key.
+        delimeters = xrange(from_ + 1, to - 1)
+        ave_entp = lambda i: (self.cnt_entp(key, from_, i), self.cnt_entp(key, i, to))
+        map(ave_entp, delimeters)
         raise NotImplementedError
 
     def get_predicate(self, from_, to):
         # 1* Iterate through a keys:
         #     2* Iterate through a sequence to get minimum entropy item.
-        # 3* Return minimum entropy key and item index
+        # 3* Git minimum entropy key and item index
+        # 4* Determine 1 0 direction and set an appropriate predicate function.
         raise NotImplementedError
 
     def learn(self):
